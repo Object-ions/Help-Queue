f2957d84ece7c2d382de884810364fb4ec0db0b3
object-ions <moshikoatia@gmail.com>
2023-11-12 10:51:22 -0800

    Add files. WIP. Follow LHTP

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import TicketList from "./components/TicketList";
+
+function App() {
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <Header />
+      <TicketList />
+    </React.Fragment>
+  );
+}
+
+export default App;
