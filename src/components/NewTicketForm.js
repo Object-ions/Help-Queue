55e35f7557eaae9ca171a9b32bc8623fea2525a0
object-ions <moshikoatia@gmail.com>
2023-11-15 10:43:57 -0800

    Add form with event listeners

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,9 +1,30 @@
 import React from "react";
 
 function NewTicketForm(props){
+
+  function handleNewTicketFormSubmission(e) {
+    e.preventDefault();
+    console.log(e.target.names.value)
+    console.log(e.target.location.value)
+    console.log(e.target.issue.value)
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit = {handleNewTicketFormSubmission}>
+        <input 
+          type='text'
+          name='names'
+          placeholder = 'Pair Names'/>
+        <input 
+          type='text'
+          name='location'
+          placeholder='Location'/>
+        <textarea 
+          name='issue'
+          placeholder='Describe Your Issue.'/>
+        <button type='submit'>Help!</button>
+      </form>
     </React.Fragment>
   );
 }
